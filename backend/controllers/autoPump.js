99e5775fba04ededfea50da80cb6396b1b89f39a
Thien Su Nha Ben <dangpquoc2022@gmail.com>
2023-12-18 21:28:36 +0700

    publish code

---
+++ backend/controllers/autoPump.js
@@ -0,0 +1,26 @@
+const { default: axios } = require('axios')
+const Observable = require('./Observer')
+const device = require('../models/device.model')
+const { turnOn, turnOff } = require('./schedule.controller')
+
+
+module.exports.autoPump = function (data) {
+    console.log('aaaaaa')
+    if(data['do-am-dat']){
+        console.log(data['do-am-dat'])
+        device.find({type:"soil",owner:data.owner})
+        .then((device) => {
+            console.log(data['do-am-dat'])
+            console.log( device[0].threshold.min)
+            if(data['do-am-dat'] < device[0].threshold.min){
+                turnOn("system");
+            }
+            else{
+                turnOff("system");
+            }
+
+        })
+        .catch((err) => console.log(err))
+    }
+}
+  
